8b814dfbe0160eb7be9ac461627d517a9121451f
RoiEf <roi.efrati@gmail.com>
2019-02-02 20:23:58 +0200

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DwD
+
 A Delay without using the delay() function for Arduino based applications
+
+"example.cpp" have several implementation examples.
+
+This library is under active development.
---
+++ dwd.h
@@ -0,0 +1,25 @@
+#ifndef __tim___
+#define __tim___
+
+#if (ARDUINO >= 100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class timer
+{
+    unsigned long currentMillis;
+    unsigned long startMillis;
+    unsigned long delta;
+
+  public:
+    timer();
+    timer(int, int);
+    void setDelta(int, int);
+    int getDelta();
+    void resetTimer();
+    bool checkInterval();
+};
+
+#endif
