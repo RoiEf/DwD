d9e9f1e6dcd563b33db611df0982953c3d84126b
RoiEf <45352222+RoiEf@users.noreply.github.com>
2019-12-02 08:53:36 +0200

    New versio. 3.0.1
    Now using a single .hpp file.
    Added auto reset function to checkInterval().
    Added getFirst().

---
+++ dwd.hpp
@@ -0,0 +1,67 @@
+#ifndef __tim301___
+#define __tim301___
+
+#if (ARDUINO >= 100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class TIMER {
+    unsigned long currentMillis;
+    unsigned long startMillis;
+    unsigned long delta;
+    bool firstTimeFlag = true;
+
+   public:
+    TIMER() {
+        delta = 1000;  // 1 sec delta
+        firstTimeFlag = 0;
+        startMillis = millis();  //initial start time for DwD
+    }
+    TIMER(unsigned long t) {
+        delta = t;
+        firstTimeFlag = 0;
+        startMillis = millis();  //initial start time for DwD
+    }
+
+    void setDelta(unsigned long t) {
+        delta = t;
+    }
+
+    int getDelta() {
+        return (int)delta;
+    }
+
+    void resetTimer() {
+        startMillis = millis();
+    }
+
+    bool checkInterval() {
+        currentMillis = millis();
+        if (currentMillis - startMillis >= delta)  //test whether the period has elapsed
+        {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    bool checkInterval(bool reset) {
+        currentMillis = millis();
+        if (currentMillis - startMillis >= delta) {  //test whether the period has elapsed
+            if (reset)
+                resetTimer();
+            return 1;
+        } else
+            return 0;
+    }
+    bool getFirst() {
+        if (firstTimeFlag) {
+            firstTimeFlag = false;
+            return true;
+        } else
+            return false;
+    }
+};
+
+#endif
